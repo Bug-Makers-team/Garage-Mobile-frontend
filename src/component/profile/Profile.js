d4572a3dab3a846e4e78937ed08b717b1e58ee2d
OmarHumamah <omar.nabeel.h@gmail.com>
2021-12-30 21:38:38 +0200

    navBar

---
+++ src/component/profile/Profile.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function Profile() {
+    return (
+        <View style={{justifyContent:'center',  alignItems: 'center', top: 200}}>
+            <Text>Profile Page</Text>
+        </View>
+    )
+}
